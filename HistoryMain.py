57c2ff8b1ecd9390e10bb4b4fd428cebf12e0f25
yuuuuki1710001 <62904489+yuuuuki1710001@users.noreply.github.com>
2020-06-29 20:22:41 +0900

    Add files via upload

---
+++ HistoryMain.py
@@ -0,0 +1,22 @@
+import pymysql
+
+conn = pymysql.connect(
+                    user='admin',
+                    passwd='10pan',
+                    db='cook',
+                    port = 3306,
+                    charset = 'utf8'
+)
+
+cur = conn.cursor()
+cur.execute('USE cook')
+
+def HistoryRegister(UserID,recipeURL,recipeTitle):
+    cur.execute('INSERT INTO favorite (UserID,recipeURL,recipeTitle) VALUES (%s,%s,%s)',(UserID, recipeURL, recipeTitle))
+    conn.commit()
+
+def HistoryDisplay(userID):
+    historyTitles = []
+    cur.execute('SELECT * FROM history WHERE UserID = %s',(userID))
+    historyTitles = [row[2] for row in cur.fetchall()]
+    return historyTitles
