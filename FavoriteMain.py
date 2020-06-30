4ca60cf18b650bad250721b06f8ab5a6f665e0d1
yuuuuki1710001 <62904489+yuuuuki1710001@users.noreply.github.com>
2020-06-30 13:59:53 +0900

    latest version

--- FavoriteMain.py
+++ FavoriteMain.py
@@ -11,20 +11,25 @@ conn = pymysql.connect(
 cur = conn.cursor()
 cur.execute('USE cook')
 
-def FavoriteRegister(UserID,recipeURL,recipeTitle):
-    cur.execute('SELECT * FROM favorite WHERE UserID = %s'
+def FavoriteRegister(userID,recipeURL,recipeTitle):
+    cur.execute('SELECT * FROM favorite WHERE userID = %s'
                'AND recipeURL = %s'
-               'AND recipeTitle = %s',(UserID, recipeURL, recipeTitle))
-    
-    if cur.rowcount == 0:
-        cur.execute('INSERT INTO favorite (UserID,recipeURL,recipeTitle) VALUES (%s,%s,%s)',(UserID, recipeURL, recipeTitle))
-        conn.commit()
-        return 0
+               'AND recipeTitle = %s',(userID, recipeURL, recipeTitle))
 
-    return 1
+    if cur.rowcount == 0 :
+        cur.execute('INSERT INTO favorite (userID,recipeURL,recipeTitle) VALUES (%s,%s,%s)',(userID, recipeURL, recipeTitle))
+        conn.commit()
+        return 1
+
+    return 0
 
 def FavoriteDisplay(userID):
     favoriteTitles = []
     cur.execute('SELECT * FROM favorite WHERE UserID = %s', (userID))
     favoriteTitles = [row[2] for row in cur.fetchall()]
     return favoriteTitles
+
+def FavoriteDelete(userID,recipeTitle):
+    cur.execute('DELETE * FROM favorite WHERE UserID = %s'
+               'AND recipeTitle = %s',(userID, recipeTitle))
+    conn.commit()
