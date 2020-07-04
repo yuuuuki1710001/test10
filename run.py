f0c43fd71a80ddb7fa21bf7624a1091f7fc14c7a
al18087 <k-noda1999@outlook.jp>
2020-07-04 21:53:21 +0900

    7th commit

---
+++ run.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from cookme import Component1, C2
+
+app = Flask(__name__)
+app.register_blueprint(C2.user)
+app.register_blueprint(Component1.cookme)
+app.config['SECRET_KEY'] = 'sample1203'
+
+if __name__ == '__main__':
+    app.run(debug=True)
