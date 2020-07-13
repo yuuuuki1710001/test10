cff69e9123b80f3d78c000a7d35cae479314be28
al18087 <k-noda1999@outlook.jp>
2020-07-14 06:28:03 +0900

    7/14 commit

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ app.register_blueprint(upload.img)
 app.config['SECRET_KEY'] = 'recipe key'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
